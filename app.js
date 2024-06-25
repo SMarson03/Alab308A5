16c7b5206a4be8771b8afc6a807aa6ef831c735b
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-24 21:20:30 -0400

    1

---
+++ app.js
@@ -0,0 +1,13 @@
+const PI = 3.1415;
+const radius = 5;
+const area = PI * radius * radius
+console.log(area)
+
+//Each plant requires a minimum space of 0.8 square meters
+
+const plantSpace = .8;
+const plant = 100;
+const newArea = area * 5
+console.log(newArea)
+
+const maxCapacity = newArea/plantSpace
